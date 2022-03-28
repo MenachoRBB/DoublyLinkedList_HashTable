85295f8545818884a06031e165cbfecf199fb745
menachorbb <ivanmenachodominguez@gmail.com>
2022-03-28 20:11:49 +0200

    Added some tests to try to search a node

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PersonalException {
 	    DoublyLinkedList<Ciutada> list = new DoublyLinkedList();
 
         Ciutada test1 = new Ciutada("Ivan", "Menacho", "48275497S");
@@ -10,6 +10,7 @@ public class Main {
         Ciutada test3 = new Ciutada("Antonio", "Matas", "46275497S");
         Ciutada test4 = new Ciutada("Ivan", "Gonzalez", "45275497S");
         Ciutada test5 = new Ciutada("Husema", "Ramos", "44275497S");
+        Ciutada test6 = new Ciutada("Error", "Error", "Error");
 
         list.addNode(test1);
         list.addNode(test2);
@@ -17,6 +18,12 @@ public class Main {
         list.addNode(test4);
         list.addNode(test5);
         list.showAll();
+
+        try{
+            System.out.println(list.search(test6));
+        }catch(PersonalException e){
+            System.err.println(e);
+        }
 
 
     }
