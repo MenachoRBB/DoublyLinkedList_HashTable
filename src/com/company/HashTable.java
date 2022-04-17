e2c6f411fa0818ef4ec6ef0142bce821841d31a0
MenachoRBB <ivanmenachodominguez@gmail.com>
2022-04-17 17:17:27 +0200

    pasar al portatil

--- src/com/company/HashTable.java
+++ src/com/company/HashTable.java
@@ -15,20 +15,25 @@ public class HashTable<K extends Comparable<K>, V extends Comparable<V>> {
     }
 
     //Add function
-    public void add(K key, V value){
+    public void add(K key, V value) throws PersonalException {
+        if (key == null)
+            throw new PersonalException("Key is null");
+        else if (value == null)
+            throw new PersonalException("Value is null");
+
         int index = key.hashCode() % hashTable.length;
 
-        if(hashTable[index] == null){
+        if (hashTable[index] == null) {
             hashTable[index] = new NodeHash<>(key, value, null);
             nElem++;
-        }else{
+        } else {
             NodeHash temp = hashTable[index];
-            while(temp != null){
-                if(temp.key.compareTo(key) == 0){   //mateixa clau, actualitzem valor
+            while (temp != null) {
+                if (temp.key.compareTo(key) == 0) {   //mateixa clau, actualitzem valor
                     temp.value = value;
                     break;
                 }
-                if(temp.next == null){
+                if (temp.next == null) {
                     temp.next = new NodeHash<>(key, value, hashTable[index]);
                     nElem++;
                 }
@@ -37,21 +42,16 @@ public class HashTable<K extends Comparable<K>, V extends Comparable<V>> {
         }
     }
 
-    //Function to get the nElems of the hash table
-    public int getnElem(){
-        return nElem;
-    }
-
     //Function to get a value by passing the key
-    public V get(K key){
+    public V get(K key) {
         int index = key.hashCode() % hashTable.length;
 
-        if(hashTable[index] == null)
+        if (hashTable[index] == null)
             return null;
-        else{
+        else {
             NodeHash temp = hashTable[index];
-            while(temp != null){
-                if(temp.key.compareTo(key) == 0){
+            while (temp != null) {
+                if (temp.key.compareTo(key) == 0) {
                     return (V) temp.value;
                 }
                 temp = temp.next;
@@ -60,25 +60,31 @@ public class HashTable<K extends Comparable<K>, V extends Comparable<V>> {
         }
     }
 
+    //Function to get the nElems of the hash table
+    public int getnElem() {
+        return nElem;
+    }
+
+
     //Function to remove a node by passing the key
-    public void remove(K key){
+    public void remove(K key) {
         int index = key.hashCode() % hashTable.length;
 
-        if(hashTable[index] == null){
+        if (hashTable[index] == null) {
             return;
-        }else{
+        } else {
             NodeHash previous = null;
             NodeHash current = hashTable[index];
 
-            while (current != null){
-                if(current.key.compareTo(key) == 0){
-                    if(previous == null){
+            while (current != null) {
+                if (current.key.compareTo(key) == 0) {
+                    if (previous == null) {
                         hashTable[index] = current.next;
-                    }else{
+                    } else {
                         previous.next = current.next;
                     }
 
-                    if(hashTable[index] == null){
+                    if (hashTable[index] == null) {
                         nElem--;
                     }
                     return;
@@ -87,6 +93,32 @@ public class HashTable<K extends Comparable<K>, V extends Comparable<V>> {
                 current = current.next;
             }
         }
+    }
+
+    //Function that check if an element is at the table
+    int search(K key) {
+        int i = 0;
+        int count = 0;
+        for (i = 0; i < hashTable.length; i++) {
+            if(hashTable[i] != null){
+                while (hashTable[i] != null){
+                    System.out.println("The value of the node is: "+hashTable[i].value);
+                    if(hashTable[i].key.compareTo(key) != 0){
+                        count++;
+                    }else{
+                        return count+1;
+                    }
+                    hashTable[i] = hashTable[i].next;
+                }
+            }
+        }
+        return count+1;
+    }
+
+
+    //Function that returns the load factor
+    double loadFactor(){
+        return hashTable.length/nElem;
     }
 
 
--- src/com/company/MainHashTable.java
+++ src/com/company/MainHashTable.java
@@ -3,20 +3,29 @@ package com.company;
 public class MainHashTable {
     public static void main(String[] args){
 
-        HashTable<Integer, String> table = new HashTable<>(3);
+        HashTable<Integer, String> table = new HashTable<>(4);
 
-        table.add(100, "test1");
-        table.add(200, "test2");
-        table.add(300, "test3");
-        table.add(100, "test4");
+        try {
+            table.add(100, "test1");
+            table.add(200, "test2");
+            table.add(300, "test3");
+            table.add(550, "test4");
+        }catch (PersonalException e){
+            System.err.println(e);
+        }
 
-        table.remove(100);
+        //table.remove(100);
 
         System.out.println("Table nElems: "+table.getnElem());
         System.out.println("The value at the key 100 is: "+table.get(100));
         System.out.println("The value at the key 200 is: "+table.get(200));
         System.out.println("The value at the key 300 is: "+table.get(300));
-       // System.out.println("The value at the key 101 is: "+table.get(101));
+        System.out.println("The value at the key 550 is: "+table.get(550));
+
+        System.out.println();
+
+        System.out.println("The cost to search the node with key 550 is: "+table.search(550));
+
 
 
 
